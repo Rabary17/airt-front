3358a3d5fa8403cba81d255da5cadf28975b0212
herriniaina <heriniaina@passion4humanity.com>
2019-03-07 21:30:49 +0200

    design css

--- src/app/admin/list-client/list-client.component.ts
+++ src/app/admin/list-client/list-client.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { ClientService } from '../../core/services/client.service';
+import { TicketsService } from '../../core';
+
 @Component({
   selector: 'app-list-client',
   templateUrl: './list-client.component.html',
@@ -7,14 +9,33 @@ import { ClientService } from '../../core/services/client.service';
 })
 export class ListClientComponent implements OnInit {
 clients: Array<any>;
+termine: number;
+restant: number;
+total: number;
 
   constructor(
-    private clientService: ClientService
+    private clientService: ClientService,
+    private ticketService: TicketsService,
   ) { }
 
   ngOnInit() {
+    const TicketTermine = [];
+    const TicketRestant = [];
+    this.ticketService.getAll().subscribe(res => {
+      res.map((ticket) => {
+        if (ticket.status === 'Terminé') {
+          TicketTermine.push(ticket);
+        } else if (ticket.status === 'En cours' || ticket.status === 'Non commencée') {
+          TicketRestant.push(ticket);
+        }
+      });
+      this.termine = TicketTermine.length;
+      this.restant = TicketRestant.length;
+      this.total = res.length;
+    });
+
     this.clientService.getAllClient().subscribe(res => {
-      // console.log(res.clients);
+      console.log('res.clients', res.clients);
       const Cli = [];
       res.clients.map( cl => {
         const TicketsTermine = [];
