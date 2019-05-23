ffa1b126d90ab88f0e0da084640d27a3df3eb4ac
unknown <rbrstart@gmail.com>
2019-05-23 16:54:22 +0300

    accueil design ok

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -7,6 +7,7 @@ import { Role } from '../core/models/role.model'
 
 @Component({
   selector: 'app-auth-page',
+  styleUrls: ['./auth.component.css'],
   templateUrl: './auth.component.html'
 })
 export class AuthComponent implements OnInit {
